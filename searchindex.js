95d902b17df82d3eec3eb7da93294a41fe8bc9c3
Amit Kushwaha <amit_kushwaha@outlook.com>
2019-03-14 15:29:43 +0100

    new pages version Thu Mar 14 15:29:43 CET 2019

--- searchindex.js
+++ searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({docnames:["sphinx-pages"],envversion:53,filenames:["sphinx-pages.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,search:0},titles:["Welcome to sphinx-pages\u2019s documentation!"],titleterms:{document:0,indic:0,page:0,sphinx:0,tabl:0,welcom:0}})
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to Image Text Reader\u2019s documentation!"],titleterms:{document:0,imag:0,indic:0,reader:0,tabl:0,text:0,welcom:0}})
